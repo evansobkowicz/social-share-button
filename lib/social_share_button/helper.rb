ceeeabd0760f6f92973e732ede97ea73fd857b4c
Evan Sobkowicz <evansobkowicz@gmail.com>
2014-06-02 11:38:07 -0400

    fixing name icon handling

--- lib/social_share_button/helper.rb
+++ lib/social_share_button/helper.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 module SocialShareButton
   module Helper
+
     def social_share_button_tag(title = "", opts = {})
       extra_data = {}
       rel = opts[:rel]
@@ -11,7 +12,7 @@ module SocialShareButton
         extra_data = opts.select { |k, _| k.to_s.start_with?('data') } if name.eql?('tumblr')
 
         link_title = t "social_share_button.share_to", :name => t("social_share_button.#{name.downcase}")
-        html << link_to("<i class='fa fa-#{name.downcase}-square'></i>".html_safe,"#", {:rel => ["nofollow", rel],
+        html << link_to("<i class='fa fa-#{get_icon(name.downcase)}-square'></i>".html_safe,"#", {:rel => ["nofollow", rel],
                                   "data-site" => name,
                                   :class => "social-share-button-#{name}",
                                   :onclick => "return SocialShareButton.share(this);",
@@ -20,5 +21,16 @@ module SocialShareButton
       html << "</div>"
       raw html.join("\n")
     end
+
+    def get_icon(name)
+      if google_plus
+        return "google-plus"
+      elsif email
+        return "envelope"
+      else
+        return name
+      end
+    end
+
   end
 end
